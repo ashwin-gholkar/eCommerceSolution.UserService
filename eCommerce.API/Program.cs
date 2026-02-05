665316210ce6b200cfc03bbf5b3eb9aa6b2caf6f
Ashwin Gholkar <ashwingholkar020@gmail.com>
2026-02-05 23:51:15 +0530

    some changes adding automapper part 1

--- eCommerce.API/Program.cs
+++ eCommerce.API/Program.cs
@@ -1,7 +1,7 @@
-using eCommerce.Infrastructure;
-using eCommerce.Core;
-using eCommerce.API.Middlewares;
 using System.Text.Json.Serialization;
+using eCommerce.API.Middlewares;
+using eCommerce.Core;
+using eCommerce.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +16,7 @@ builder.Services.AddControllers().AddJsonOptions(opt =>
     opt.JsonSerializerOptions.Converters.Add(
         new JsonStringEnumConverter());
 });
+//builder.Services.AddAutoMapper(typeof(eCommerce.Core.Mappers.ApplicationUserMappingProfile).Assembly);
 
 var app = builder.Build();
 
